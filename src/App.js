be1d078dd7eb27b9879921eaa25512ff741d35e5
tae2j <jeong6425@naver.com>
2024-03-13 22:40:06 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [글제목, b] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <h4 style={{color:'red', fontSize : '18px'}}>{post}</h4>
     </div>
   );
 }
